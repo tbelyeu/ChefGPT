77b17e6a24f3a87be61d7d800f51397e55cdacea
tbelyeu <tylerdbelyeu@gmail.com>
2023-09-15 12:52:34 -0700

    Added "not yet implemented" tool tip for facebook and apple sign in

--- lib/pages/authentication/widgets/sign_in_button.dart
+++ lib/pages/authentication/widgets/sign_in_button.dart
@@ -20,31 +20,35 @@ class SignInButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      onPressed: isEnabled ? onPressed : null,
-      style: ButtonStyle(
-        enableFeedback: isEnabled ? true : false,
-        backgroundColor: isEnabled
-            ? const MaterialStatePropertyAll(light)
-            : MaterialStatePropertyAll(Colors.grey.shade300),
-        padding: MaterialStateProperty.all(const EdgeInsets.all(16)),
-        fixedSize: MaterialStateProperty.all(Size(width, 46)),
-        side: MaterialStateProperty.all(const BorderSide(color: lightGrey)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            image,
-            width: platformController.isMobile.value ? 64 : 32,
-          ),
-          const SizedBox(width: 20),
-          CustomText(
-            text: text,
-            color: dark,
-            size: platformController.isMobile.value ? 16 : 20,
-          ),
-        ],
+    return Tooltip(
+      message: isEnabled ? "" : "Not yet implemented",
+      waitDuration: const Duration(milliseconds: 500),
+      child: OutlinedButton(
+        onPressed: isEnabled ? onPressed : null,
+        style: ButtonStyle(
+          enableFeedback: isEnabled ? true : false,
+          backgroundColor: isEnabled
+              ? const MaterialStatePropertyAll(light)
+              : MaterialStatePropertyAll(Colors.grey.shade300),
+          padding: MaterialStateProperty.all(const EdgeInsets.all(16)),
+          fixedSize: MaterialStateProperty.all(Size(width, 46)),
+          side: MaterialStateProperty.all(const BorderSide(color: lightGrey)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              image,
+              width: platformController.isMobile.value ? 64 : 32,
+            ),
+            const SizedBox(width: 20),
+            CustomText(
+              text: text,
+              color: dark,
+              size: platformController.isMobile.value ? 16 : 20,
+            ),
+          ],
+        ),
       ),
     );
   }
